2332d60b955a4ec41f0e536757464bb79ece5aad
atevsl <3380033@ukr.net>
2022-11-24 21:04:11 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,25 +5,22 @@ const emailEl = document.querySelector('[name="email"]');
 const messageEl = document.querySelector('[name="message"]');
 
 const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
 
 formEL.addEventListener('input', throttle(saveForm, 1000));
 formEL.addEventListener('submit', onSubmitForm);
 
 loadForm();
 
-function saveForm() {
-  const formData = {};
-  formEL.addEventListener('input', e => {
-    formData[e.target.name] = e.target.value;
-    console.log(formData);
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-  });
+function saveForm(e) {
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
 function onSubmitForm(event) {
   event.preventDefault();
   event.target.reset();
-  console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+  console.log('Storage:', JSON.parse(localStorage.getItem(STORAGE_KEY)));
   localStorage.removeItem(STORAGE_KEY);
 }
 
